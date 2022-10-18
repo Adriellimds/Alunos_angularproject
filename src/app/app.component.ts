dad31a15268a4a445d11f6b38f487063d603bf07
Adriellimds <adriellimds@gmail.com>
2022-10-18 10:49:07 -0300

    babysteps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Alunos_projAngular';
+  title = 'Cadastro de Alunos';
 }
